5e76a35b77c2d47155f39ddbf56aab58ac6845f9
mugman <53713683+mugman174@users.noreply.github.com>
2024-04-27 00:43:12 -0700

    Update astro.config.mjs for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "mugman174.github.io",
+  base: "new-website"
 });
